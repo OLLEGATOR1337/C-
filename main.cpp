65904028b46451092ee7aeb442e5d9a72bec24bc
OLLEGATOR1337 <olejka.berek@gmail.com>
2024-10-13 14:04:23 +0300

    add .

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 int main() 
 {
-	std::cout << "Hello World";
-	
+	int i;
+	i = 69;
+	int number = 72;
+	i += number;
+	std::cout << (i) << std::endl;
+	int array[3];
+	int* iPointer = &i;
+	int* pn = &number;
+	std::cout << (iPointer) << std::endl;
+	*iPointer = 15;
+	std::cout << (i) << std::endl;
+	iPointer = &array[0];
+	std::cout << iPointer;
 }
